5ba18a97eb70fd0fd98af6b00d060359904fc95d
ashhadraza32 <ashhadraza32@gmail.com>
2025-11-04 15:20:57 +0530

    Hashing Theory and practice problem

---
+++ HashingPractice/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HashingPractice;
+
+Console.WriteLine("Hello, World");
+
+Problem1_Sol obj = new Problem1_Sol();
+
+int[] arr = { 1, 3, 2, 4, 1, 3, 2, 5, 3, 2 };
+obj.PrintArrayElementFreq(arr);
+Console.ReadLine();
