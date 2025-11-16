e1852c4f88ad1e32d50df3e92a3c9a73615bbd5a
ashhadraza32 <ashhadraza32@gmail.com>
2025-11-16 23:28:11 +0530

    Array Problems

---
+++ ArrayProblems/Problem3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayProblems
+{
+    public class Problem3
+    {
+        public void CheckSortedArray()
+        {
+            int[] arr = { 1, 2,3,4,5 };
+            var res = IsSortedArray(arr);
+            Console.WriteLine("Array is Sorted:" +res);
+        }
+
+        public static bool IsSortedArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] > arr[i + 1])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ ArrayProblems/Problem5.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayProblems
+{
+    public class Problem5
+    {
+        public void MoveZeoresToArrayEnd()
+        {
+            int[] arr = { 16, 3, 7, 8, 0, 5, 0, 4 };
+            MoveZeroesToEnd(arr);
+        }
+        private static void MoveZeroesToEnd(int[] arr)
+        {
+            int idx = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] != 0)
+                {
+                    arr[idx] = arr[i];
+                    idx++;
+                }
+
+            }
+            while (idx < arr.Length)
+            {
+                arr[idx] = 0;
+                idx++;
+            }
+
+            Console.Write("Array after moving zeroes to End:");
+            foreach (var ele in arr)
+            {
+                Console.Write(ele+" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
